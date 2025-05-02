4b7a9641043b97cd107c11b8c35f7b8d8bf14198
= <bogdanzaharchenko7@gmail.com>
2025-05-02 14:13:02 +0200

    Add time validation

--- api/events/serializers.py
+++ api/events/serializers.py
@@ -33,6 +33,17 @@ class EventSerializer(serializers.ModelSerializer):
             'end_time',
         ]
 
+    def validate(self, data):
+        start_time = data.get('start_time')
+        end_time = data.get('end_time')
+
+        if start_time and end_time:
+            if end_time <= start_time:
+                raise serializers.ValidationError(
+                    "End time must be after start time."
+                )
+        return data
+
     def to_representation(self, instance):
         rep = super(EventSerializer, self).to_representation(instance)
         rep['city'] = instance.city.name
