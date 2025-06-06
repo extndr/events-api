d474c5434747578369529f257ea635634b51051c
dead1x <bogdanzaharchenko7@gmail.com>
2025-06-06 18:06:49 +0200

    Seed initial data on startup

--- Dockerfile
+++ Dockerfile
@@ -13,3 +13,5 @@ COPY . /app
 COPY entrypoint.sh /entrypoint.sh
 
 RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Running Database Migrations"
 python manage.py migrate
 
+echo "Seeding data (countries, cities)..."
+python seed_data.py || { echo "Data seeding failed"; exit 1; }
+
 echo "Running server..."
 python manage.py runserver 0.0.0.0:8000
 
