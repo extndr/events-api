4bd06d377933946e1ebd00be1b299c3c4f5ab21c
dead1x <bogdanzaharchenko7@gmail.com>
2025-06-08 18:49:39 +0200

    Fix issue #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+set -e
+
 echo "Running Database Migrations"
 python manage.py migrate
 
-echo "Seeding data (countries, cities)..."
-python seed_data.py || { echo "Data seeding failed"; exit 1; }
+if [ "$DJANGO_ENV" = "prod" ]; then
+  echo "Collecting static files..."
+  python manage.py collectstatic --noinput
+fi
 
-echo "Running server..."
+echo "Seeding data (countries, cities)..."
+python seed_data.py
+
+echo "Starting server..."
 python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
