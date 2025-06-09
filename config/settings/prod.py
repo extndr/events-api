0baed6d17aa72b1cb09569f93b5f418105769485
dead1x <bogdanzaharchenko7@gmail.com>
2025-06-09 22:46:33 +0200

    Implement wait_for_db logic, minor improvements

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -11,11 +11,11 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': config('DB_NAME', 'postgres'),
+        'USER': config('DB_USER', 'postgres'),
+        'PASSWORD': config('DB_PASSWORD', ''),
+        'HOST': config('DB_HOST', 'localhost'),
+        'PORT': config('DB_PORT', 5432),
     }
 }
 
