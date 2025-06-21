367d7f823235a11fd3147f33cbdfce544abdab31
dead1x <bogdanzaharchenko7@gmail.com>
2025-06-21 13:31:38 +0200

    Setup mailhog(local) and sendgrid(prod)

--- config/settings/local.py
+++ config/settings/local.py
@@ -18,11 +18,10 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# Email (MailHog)
+# https://github.com/mailhog/MailHog
 
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='fallback_email@example.com')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='fallback_password')
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST', default='localhost')
+EMAIL_PORT = env('EMAIL_PORT', default=1025)
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='dev@example.com')
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -18,3 +18,13 @@ DATABASES = {
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
     'rest_framework.renderers.JSONRenderer',
 )
+
+
+# Email (SendGrid)
+# https://sendgrid.com/docs/
+
+EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
+SENDGRID_API_KEY = env('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+SENDGRID_ECHO_TO_STDOUT = False
